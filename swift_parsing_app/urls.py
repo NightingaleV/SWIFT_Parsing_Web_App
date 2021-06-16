15e095e6057c37fff0f100e08c57dc3207026c1e
Vitezslav Slavik <vitezslav.slavik@pwc.com>
2021-06-16 18:15:50 +0200

    Create sample objects

--- swift_parsing_app/urls.py
+++ swift_parsing_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from .views import IndexView
-
+from .views import IndexView, CustomerCreateView, CustomerList
 
 app_name = 'swift_parsing_app'
 urlpatterns = [
     path('', IndexView.as_view(), name='home-page'),
+    path('customer-list/', CustomerList.as_view(), name='customer-list'),
+    path('create-customer/', CustomerCreateView.as_view(), name='create-customer'),
 ]
--- swift_parsing_app/views.py
+++ swift_parsing_app/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic import FormView, UpdateView, DetailView, ListView, CreateView
+from django.urls import reverse_lazy
+from .models import Customer
 
 
 # Create your views here.
-
 class IndexView(TemplateView):
     template_name = 'pages/index.html'
+
+
+class CustomerList(ListView):
+    model = Customer
+    context_object_name = 'customer_list'
+    template_name = 'pages/customer_list.html'
+    # paginate_by = 10
+
+
+class CustomerCreateView(CreateView):
+    model = Customer
+    fields = ['full_name']
+    template_name = 'pages/create_customer.html'
+    success_url = reverse_lazy('swift_parsing_app:customer-list')
